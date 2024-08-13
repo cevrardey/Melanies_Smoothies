201e6a2f24c1ed0d7635d3cce4ab512b0fdef19f
cevrardey <curt.evrard@ey.com>
2024-08-13 11:21:37 -0800

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -46,8 +46,7 @@ if ingredients_string:
    session.sql(my_insert_stmt).collect()
 
    st.success('Your Smoothie is ordered!', icon="✅")
-# new section to display nutrition information 
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
 fv_df = st.dataframe(data= fruityvice_response.json(), use_container_width =True)
