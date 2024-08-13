ab3b2ff3ded7573cbf153a35ce9b1b0338de3db4
cevrardey <curt.evrard@ey.com>
2024-08-13 12:03:44 -0800

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -30,9 +30,8 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
             ingredients_string += fruit_chosen + ' '
-            st.subheader(fruit_chosen+ ' Nutrition Information')
-            import requests
-            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"+ fruit_chosen)
+            st.subheader(fruit_chosen + ' Nutrition Information')
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
             fv_df = st.dataframe(data= fruityvice_response.json(), use_container_width =True)
 
 st.write(ingredients_string)
@@ -52,6 +51,6 @@ if ingredients_string:
 
    st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fv_df = st.dataframe(data= fruityvice_response.json(), use_container_width =True)
+#import requests
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#fv_df = st.dataframe(data= fruityvice_response.json(), use_container_width =True)
